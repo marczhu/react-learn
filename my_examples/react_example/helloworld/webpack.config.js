35ebf8dc504398da648e4f847fd85d8d56fb6a17
kevin <zhumiaowen@gmail.com>
2016-08-04 18:28:27 +0800

    fix webpack.config.js

--- my_examples/react_example/helloworld/webpack.config.js
+++ my_examples/react_example/helloworld/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.jsx$/, loaders: ['jsx?harmony'] }
+            { test: /\.jsx$/, loaders: ['jsx?harmony'] },
+	    { test: /\.js$/, loaders:['babel']}
         ]
     }
 }
